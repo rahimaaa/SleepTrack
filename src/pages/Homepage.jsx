98c37dc53d05605c81534a15cf9f52944c5b9a2a
rahimaaa <rakhimakhabib@gmail.com>
2023-10-21 11:24:54 -0400

    set up tailwind, and othe dependencies, and react router with the homepage

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Homepage(){
+
+    return(
+     <div className="bg-purple-800 h-screen flex items-center justify-center">
+            <h1 className="text-white font-bold text-2xl ">Ur mom</h1>
+
+     </div>
+
+    );
+}
+
+export default Homepage;
